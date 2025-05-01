26424f280de767582b4cfbb909e581c22afac5ba
Ahmed Essam <ahmedessam1427@hotmail.com>
2025-05-01 18:35:58 +0300

    Update Trainee.cpp

--- Trainee.cpp
+++ Trainee.cpp
@@ -1,17 +1,5 @@
 #include "Trainee.h"
 
-<<<<<<< HEAD
-Trainee::Trainee(string name, string dob, string email, string password, string gender,
-                 string phone, string type, bool gymOrPadel, bool assigned,
-                 float userHeight, float userWeight, float userCalPerMin, Subscription sub)
-    : User(name, dob, email, password, gender, phone, type, gymOrPadel, assigned), subscription(sub)
-{
-    this->height = userHeight;
-    this->weight = userWeight;
-    this->calPerMin = userCalPerMin;
-    this->subscription = sub;
-}
-=======
 int Trainee::id = 0;
 
 Trainee::Trainee() = default;
