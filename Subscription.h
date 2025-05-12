8e139f4c4ea7904786c25952017e33e2e1f77501
Ahmed Essam <ahmedessam1427@hotmail.com>
2025-05-12 20:39:08 +0300

    Add files via upload

--- Subscription.h
+++ Subscription.h
@@ -1,36 +1,35 @@
-//#pragma once
-//#include <iostream>
-//using namespace std;
-//
-//class Subscription {
-//	static int id;
-//	int subId;
-//    string startDate, endDate;
-//    int period; //period[1 year, 3 month as the user likes and the price will be calculated by a month cost]
-//    bool type; //type[annually - 1, monthly - 0]
-//    float price;
-//
-//    public:
-//    Subscription();
-//    Subscription(string startDate, bool type, int period);
-//    ~Subscription();
-//
-//    int getID();
-//    string getStartDate();
-//    string getEndDate();
-//    bool getType();
-//    int getPeriod();
-//    float getPrice();
-//
-//    void setStartDate(string startDate);
-//    void setEndDate(string endDate);
-//    void setType(bool type);
-//    void setPeriod(int period);
-//    void setPrice(float price);
-//
-//    string end_date_calc(string startDate, bool type, int period);
-//    float manage_discount(float price);
-//    void display();
-//
-//};
-//
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Subscription {
+	static int id;
+	int subId;
+    string startDate, endDate;
+    int period; //period[1 year, 6 months, 3 months] only 3 values (1,6,3)
+    float price;
+
+    public:
+    Subscription();
+    Subscription(string startDate, int period);
+    ~Subscription();
+
+    int getID();
+    string getStartDate();
+    string getEndDate();
+    int getPeriod();
+    float getPrice();
+
+    void setStartDate(string startDate);
+    void setEndDate(string endDate);
+    void setPeriod(int period);
+    void setPrice(float price);
+
+    string end_date_calc(string startDate, int period);
+    float manage_discount(float price);
+    void display();
+	int return_year(string date);
+	int return_month(string date);
+
+};
+
